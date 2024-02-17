52a34430d806df88fe5d3d77f6bbcaf77b845c4c
Ayush Singh <142167916+ayush-singh09@users.noreply.github.com>
2024-02-17 17:14:42 +0530

    Added Length Function

--- src/Components/Users.jsx
+++ src/Components/Users.jsx
@@ -9,7 +9,7 @@ function Users() {
     <div className="bg-blue-300 h-fit w-full p-10">
       <h1 className="font-bold text-5xl">Users</h1>
       <div className="p-5 flex flex-col gap-2">
-        {data.map((user,i)=>{
+        {data.length > 0 ? data.map((user,i)=>{
             return <Link
             key={i}
             to={`/users/${user.id}`}
@@ -17,7 +17,7 @@ function Users() {
           >
             {user.name}
           </Link>
-        })}
+        }) : null}
       </div>
     </div>
   );
